683a18b8c870838f7abb9f5a81c2d270ea38c6b7
szq <100530047@qq.com>
2023-01-04 16:22:31 +0800

    11

---
+++ nacos-spring-cloud-example/spring-cloud-study01/src/main/java/com/example/springcloudstudy01/SpringCloudStudy01Application.java
@@ -0,0 +1,13 @@
+package com.example.springcloudstudy01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStudy01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStudy01Application.class, args);
+    }
+
+}
